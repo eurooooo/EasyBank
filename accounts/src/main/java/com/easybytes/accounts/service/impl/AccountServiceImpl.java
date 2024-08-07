d8866e46852c26fe0d4801ce049277520c668ef3
Zephyr Lin <lzh910954163@gmail.com>
2024-08-07 17:05:52 +0800

    createAccount api

---
+++ accounts/src/main/java/com/easybytes/accounts/service/impl/AccountServiceImpl.java
@@ -0,0 +1,57 @@
+package com.easybytes.accounts.service.impl;
+
+import com.easybytes.accounts.Repository.AccountsRepository;
+import com.easybytes.accounts.Repository.CustomerRepository;
+import com.easybytes.accounts.constants.AccountsConstants;
+import com.easybytes.accounts.dto.CustomerDto;
+import com.easybytes.accounts.entity.Accounts;
+import com.easybytes.accounts.entity.Customer;
+import com.easybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.easybytes.accounts.mapper.CustomerMapper;
+import com.easybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountServiceImpl implements IAccountsService {
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    /**
+     * @param customerDto
+     */
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer =  customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobile number " + customerDto.getMobileNumber());
+        }
+        customer.setCreatedBy("Anonymous");
+        customer.setCreatedAt(LocalDateTime.now());
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    /**
+     * @param customer
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedBy("Anonymous");
+        newAccount.setCreatedAt(LocalDateTime.now());
+        return newAccount;
+    }
+}
